3acf5fd6d7cb2f859eb23bb367e11b2b344db30b
Jared <jaredtrp@gmail.com>
2022-01-30 13:23:59 -0600

    added directories for lib and routes

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const htmlRoutes = require('./routes/htmlRoutes');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('./public'));
+
+// Routes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
